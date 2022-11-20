beeb117097515ab2e476f8b104e2fda7ae8061ef
“victornnamdii” <victorilodiuba@gmail.com>
2022-11-20 18:30:36 +0100

    Task 12: HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,8 +3,9 @@
     Starts a flask application
 """
 
-from api.v1.views import app_views, jsonify
-from flask import Flask
+from api.v1.views import app_views
+from flask_cors import CORS
+from flask import Flask, jsonify
 from models import storage
 from os import getenv
 
@@ -15,6 +16,7 @@ port = getenv('HBNB_API_PORT') if getenv('HBNB_API_PORT') else '5000'
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={'/*': {'origins': '0.0.0.0'}})
 
 
 @app.errorhandler(404)
